e9d3e3eb7740cab1fb54344771d204f1e05bba85
Shoxza <benjaminhughes19@hotmail.com>
2025-04-24 22:49:36 +0200

    First Revision of the quiz script

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const hidden_elements = document.querySelectorAll('.hide')
+const observer = new IntersectionObserver(elements => {
+    elements.forEach(element => {
+        if(element.isIntersecting){
+            element.target.classList.add('show');
+        }
+        else{
+            element.target.classList.remove('show')
+        }
+    })
+});
 
-
-
+hidden_elements.forEach(el => {
+    observer.observe(el);
+});
+// Script collected from: https://javascript.plainenglish.io/how-to-implement-animation-on-scroll-with-vanilla-javascript-655093a38059
 const waterQuiz = [
     {
       question: "How many liters does a 10 minute Shower use?",
@@ -25,22 +38,43 @@ const waterQuiz = [
     
   ];
 
-const hidden_elements = document.querySelectorAll('.hide')
-const observer = new IntersectionObserver(elements => {
-    elements.forEach(element => {
-        if(element.isIntersecting){
-            element.target.classList.add('show');
-        }
-        else{
-            element.target.classList.remove('show')
-        }
-    })
+let score = 0;
+let guessedAmount = 0;
+let questionCount = 0;
+const normalAmount = 396;
+const alexAmount = 552;
+
+// Selecting Buttons inside question containers
+const questionGroups = document.querySelectorAll(".questions .button");
+
+questionGroups.forEach(function(question, i) {
+  // Get all buttons in container
+  const buttons = question.querySelectorAll("button");
+
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function()  {
+      // Exit Function if question has already been answered
+      if (question.dataset.answered === "true") return;
+      // Set as true if it isn't already
+      question.dataset.answered = "true";
+
+      // Converting string to numbers so strict equality works correctly
+      const clickedValue = parseInt(button.innerText, 10);
+      const correctAnswer = waterQuiz[i].answer;
+      guessedAmount += clickedValue;
+
+      if (clickedValue === correctAnswer) {
+        button.classList.add("correct");
+        score++;
+      } else {
+        button.classList.add("false");
+      }
+      // Logging to see the results of the script working
+      console.log(guessedAmount);
+      console.log(score);
+    });
+  });
 });
 
-hidden_elements.forEach(el => {
-    observer.observe(el);
-});
 
-
-// Script collected and edited from: https://javascript.plainenglish.io/how-to-implement-animation-on-scroll-with-vanilla-javascript-655093a38059
 
